f7165a5fc6c55722bcce105e8b85ebefd8fb5834
差一点成帅哥 <57213312+linjia1993912@users.noreply.github.com>
2020-11-03 11:22:25 +0800

    首次提交

---
+++ HELP.md
@@ -0,0 +1,1 @@
+#该项目练习搭建springboot+SpringCloud架构
---
+++ src/main/java/com/example/springbootspringcloudarchitecture/SpringbootSpringcloudArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootspringcloudarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringcloudArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringcloudArchitectureApplication.class, args);
+	}
+
+}
