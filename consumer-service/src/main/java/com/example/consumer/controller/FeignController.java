c40175eae001b929f0520521255ffeb05ec8ff10
Administrator <linjiait@sina.com>
2020-11-05 21:32:23 +0800

    1.consumer-service Controller增加端口区分 2.zuul增加负载配置

--- consumer-service/src/main/java/com/example/consumer/controller/FeignController.java
+++ consumer-service/src/main/java/com/example/consumer/controller/FeignController.java
@@ -2,6 +2,7 @@ package com.example.consumer.controller;
 
 import com.example.consumer.service.IFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class FeignController {
+
+    @Value("${server.port}")
+    String port;
+
     //编译器报错，无视。 因为这个Bean是在程序启动的时候注入的，编译器感知不到，所以报错。
     @Autowired
     private IFeignService iFeignService;
@@ -27,13 +32,13 @@ public class FeignController {
      **/
     @GetMapping("/hi")
     public String sayHi(@RequestParam(value = "name", defaultValue = "forezp") String name) {
-        System.out.println("----------feign-service");
+        System.out.println("----feign-service----"+port);
         return iFeignService.home(name);
     }
 
     @GetMapping("/getConfig")
     public String getConfig() {
-        System.out.println("----------feign-service");
+        System.out.println("----feign-service----"+port);
         return iFeignService.getConfig();
     }
 }
