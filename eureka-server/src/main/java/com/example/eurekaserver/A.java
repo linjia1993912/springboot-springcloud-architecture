625c5ff27d380cd0407013662dbedc4f35533cfc
linjia <linjiait@sina.com>
2020-11-24 14:52:39 +0800

    1.测试

---
+++ eureka-server/src/main/java/com/example/eurekaserver/A.java
@@ -0,0 +1,14 @@
+package com.example.eurekaserver;
+
+/**
+ * @Description:
+ * @Author LinJia
+ * @Date 2020/11/24
+ **/
+public class A {
+
+
+    public static void main(String[] args) {
+
+    }
+}
